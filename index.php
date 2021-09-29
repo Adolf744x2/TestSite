f94b0a0b0577c7ff95c73f875233f2810c64fdd6
Adolf <den.denisovich.14@bk.ru>
2021-09-29 10:01:04 +1000

    jhfgjh

--- index.php
+++ index.php
@@ -1,64 +1,83 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
-     rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" 
-     crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>Hello, world!</title>
 </head>
 <body>
-    <?php
-    function calc(){
-        if_isset($_REQUEST['btnCalc']){
-            echo 'hasgdf'
+<?php
+function calc(){
+    if(isset($_POST['btnCalc'])){
+        $message = "нечего считать(((";
+        if (isset($_POST['a']) && !empty($_POST['a']))
+        {
+            $a = $_POST['a'];
+        };
+        if (isset($_POST['b']) && !empty($_POST['b']))
+        {
+            $b = $_POST['b'];
+        };
+        if (isset($_POST['action']) && !empty($_POST['action']))
+        {
+            $action = $_POST['action'];
+        };
+        switch ($action){
+            case '-': $message = "$a - $b= ";$message .= $a-$b; break;
+            case '+': $message = "$a + $b= ";$message .=  $a+$b;break;
+            case '/': $message = "$a / $b= ";$message .=  $a/$b;break;
+            case '*': $message = "$a * $b= ";$message .=  $a*$b;break;
+            default: $message = "WTF? ";
         }
     }
-    ?>
+    return $message;
+}
+?>
+
 <div class="container">
+
 <h1>Калькулятор v0.01</h1>
 
 <div class="row">
-<div class="col-6">
-    
+    <div class="col-6">
+        <form action="calc.php" method="post">
+            <div class="mb-3" id="a1">
+                <label  class="form-label">введите число A</label>
+                <input type="text" class="form-control" name="a">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">введите число B</label>
+                <input type="text" class="form-control" name="b">
+            </div>
+            <div class="mb-3">
+                <select class="form-select" name="action">
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="*">*</option>
+                    <option value="/">/</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary" name="btnCalc">Посчитать</button>
+        </form>
+    </div>
+    <div class="col-6">
+        <div class="card">
+            <div class="card-body">
+                Результат)<br>
+                <?php echo calc();
 
+                ?>
 
-    <form>
-<div class="mb-3">
-    <label class="form-labl"> введите первое число</label>
-    <input type="text" class="form-control" name="a">
+            </div>
+        </div>
+    </div>
 </div>
-<div class="mb-3">
-    <label class="form-labl"> введите второе число</label>
-    <input type="text" class="form-control" name="b">
-</div>
-</div>
-
-<select class="from-select" name="action">
-    
-<option value="+">+</option>
-<option value="-">-</option>
-<option value="*">*</option>
-<option value="/">/</option>
-</select>
-
-<button type="submit" class="btn-primary" name="btnCalc"> Посчитать</button>
-    </form>
-
-</div>
-<div>
-
-</div class="col-6">
-
-
 
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" 
-crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 
 </body>
 </html>
